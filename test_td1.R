9bc1e6e7037e2b3af4d19156d4809155b0683616
flavusongithub <99904057+flavusongithub@users.noreply.github.com>
2023-01-27 16:56:08 +0100

    Update test_td1.R

---
+++ test_td1.R
@@ -0,0 +1,20 @@
+#objet simple
+x <- 2
+x
+y<-50
+y
+
+#vecteur
+taille1 <- 140
+taille2 <- 194
+taille3 <- 147
+taille4 <- 187
+taille5 <- 163
+(taille1 + taille2 + taille3 + taille4 + taille5) / 5
+
+tailles <- c(165, 146, 179, 174, 137, 199)
+tailles
+
+tailles_m <-tailles/100
+tailles_m
+
